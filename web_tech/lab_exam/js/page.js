e379659ce3d43f8efb751d027e45e402529c3ed3
omi-otw <sayemomi896@gmail.com>
2026-03-08 11:59:26 +0600

    commit

---
+++ web_tech/lab_exam/js/page.js
@@ -0,0 +1,18 @@
+<script>
+        function validateForm() {
+            var fields = ['firstName', 'lastName', 'email', 'mobile', 'message'];
+            for (var i = 0; i < fields.length; i++) {
+                var value = document.getElementById(fields[i]).value.trim();
+                if (value === '') {
+                    alert("Field Value need to be filled up");
+                    return false;
+                }
+            }
+            console.log("First Name: " + document.getElementById('firstName').value);
+            console.log("Last Name: " + document.getElementById('lastName').value);
+            console.log("Email: " + document.getElementById('email').value);
+            console.log("Mobile Number: " + document.getElementById('mobile').value);
+            console.log("Message: " + document.getElementById('message').value);
+            return false; // Prevent form from submitting to a server
+        }
+    </script>
